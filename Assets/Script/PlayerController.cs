d0e291731292285445c6f72b9a1633437b1a1ca1
dongpham35 <81824832+dongpham35@users.noreply.github.com>
2023-10-26 23:38:30 +0700

    update

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,45 +6,42 @@ public class PlayerController : MonoBehaviour
 {
     public string inputHorizontal;
     public string inputVertical;
-    private float dirX = 0f;
-    private float speedCharacter = 7f;
-    private float jumpCharacter = 14f;
+
+    private float dirX;
+    private float speedCharacter = 7.0f;
+    private float jumpCharacter = 14.0f;
     private Rigidbody2D rb;
-    private Animator aniPlayer;
+    private Animator animatorPlayer;
     private SpriteRenderer spritePlayer;
-    enum anim { idle, run, jump, fall, doublejump }
-    private anim state;
+    enum anim { idle, run, jump, fall, doublejump}
+    anim state;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        aniPlayer = GetComponent<Animator>();
+        animatorPlayer = GetComponent<Animator>();
         spritePlayer = GetComponent<SpriteRenderer>();
     }
 
-
     private void Update()
     {
-        
         dirX = Input.GetAxisRaw(inputHorizontal);
         rb.velocity = new Vector2(dirX * speedCharacter, rb.velocity.y);
-        if (Input.GetButtonDown(inputVertical))
+        if(Input.GetButtonDown(inputVertical))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpCharacter);
         }
-        UpdateAnimator();
+
+        updateAnimation();
     }
 
-    private void UpdateAnimator()
+    private void updateAnimation()
     {
-        
-
-        if (dirX > 0)
+        if(dirX > 0f)
         {
             spritePlayer.flipX = false;
             state = anim.run;
-        }
-        else if (dirX < 0)
+        }else if(dirX < 0f)
         {
             spritePlayer.flipX = true;
             state = anim.run;
@@ -54,16 +51,17 @@ public class PlayerController : MonoBehaviour
             state = anim.idle;
         }
 
-        if (rb.velocity.y > 0f)
+        if(rb.velocity.y > 0f)
         {
             state = anim.jump;
         }
 
-        if (rb.velocity.y < 0f)
+        if(rb.velocity.y < 0f)
         {
             state = anim.fall;
         }
 
-        aniPlayer.SetInteger("state", (int)state);
+        animatorPlayer.SetInteger("state", (int) state);
     }
+
 }
