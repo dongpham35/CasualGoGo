24fbb9d72770b9e077d275ef83b9dce85fcdf204
dongpham35 <81824832+dongpham35@users.noreply.github.com>
2023-10-26 15:50:17 +0700

    update

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,21 +10,59 @@ public class PlayerController : MonoBehaviour
     private float speedCharacter = 7f;
     private float jumpCharacter = 14f;
     private Rigidbody2D rb;
-    void Start()
+    private Animator aniPlayer;
+    private SpriteRenderer spritePlayer;
+    enum anim { idle, run, jump, fall, doublejump }
+    private anim state;
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        aniPlayer = GetComponent<Animator>();
+        spritePlayer = GetComponent<SpriteRenderer>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
 
-    void Update()
+    private void Update()
     {
         dirX = Input.GetAxisRaw(inputHorizontal);
         rb.velocity = new Vector2(dirX * speedCharacter, rb.velocity.y);
-
         if (Input.GetButtonDown(inputVertical))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpCharacter);
         }
+
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+
+        if (dirX > 0)
+        {
+            spritePlayer.flipX = false;
+            state = anim.run;
+        }
+        else if (dirX < 0)
+        {
+            spritePlayer.flipX = true;
+            state = anim.run;
+        }
+        else
+        {
+            state = anim.idle;
+        }
+
+        if (rb.velocity.y > 0f)
+        {
+            state = anim.jump;
+        }
+
+        if (rb.velocity.y < 0f)
+        {
+            state = anim.fall;
+        }
+
+        aniPlayer.SetInteger("state", (int)state);
     }
 }
