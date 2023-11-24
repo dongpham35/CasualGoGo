7e6174de55bdd1d89d65e3e0053bcca4bab40247
dongpham35 <81824832+dongpham35@users.noreply.github.com>
2023-11-25 00:44:34 +0700

    update

--- Assets/Script/CountScore.cs
+++ Assets/Script/CountScore.cs
@@ -5,21 +5,25 @@ using UnityEngine.UI;
 
 public class CountScore : MonoBehaviour
 {
-    [SerializeField] private Text txtTime;
+    private Text txtTime;
     public static float runtime;
     private float startTime;
     private bool timeRunning;
+    private float minutes = 0.0f;
+    private float seconds = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         runtime = 0.0f;
         startTime = 0;
+        GameObject canvas = transform.Find("Timer").gameObject as GameObject;
+        txtTime = canvas.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timeRunning)
+        if (timeRunning)
         {
             runtime = Time.time - startTime;
             UpdateTime(runtime);
@@ -28,14 +32,24 @@ public class CountScore : MonoBehaviour
 
     private void UpdateTime(float time)
     {
-        float minutes = Mathf.FloorToInt(time / 60);
-        float seconds = Mathf.FloorToInt(time % 60);
-        txtTime.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        minutes = Mathf.FloorToInt(time / 60);
+        seconds = Mathf.FloorToInt(time % 60);
+        txtTime.text = string.Format(format:"{0:00}:{1:00}", minutes, seconds);
     }
-
-    public void Reset()
+    public void ResetTimer()
     {
         startTime = 0;
-        UpdateTime(0f);
+        UpdateTime(0.0f);
+    }
+
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        timeRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        timeRunning = false;
     }
 }
